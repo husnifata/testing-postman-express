6051b3334baef0a356e96cc787f86dbd509198ee
husnifatah <husnifatah0@gmail.com>
2025-08-01 17:30:28 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let clubs = [
+  { id: 1, name: "Arsenal", stadium: "Emirates" },
+  { id: 2, name: "Liverpool", stadium: "Anfield" },
+  { id: 3, name: "New Castle", stadium: "St.James" },
+];
+
+app.get("/clubs", (req, res) => {
+  res.json(clubs);
+});
+
+app.get("/clubs/:id", (req, res) => {
+  const club = clubs.find((c) => c.id == parseInt(req.params.id));
+  if (!club) return res.status(404).json({ message: "Club not found" });
+  res.json(club);
+});
+
+app.post("/clubs", (req, res) => {
+  const newClub = { id: Date.now(), ...req.body };
+  clubs.push(newClub);
+  res.status(201).json(newClub);
+});
+
+app.put("/clubs/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const clubIndex = clubs.findIndex((c) => c.id === id);
+  if (clubIndex === -1)
+    return res.status(404).json({ message: "Club Not Found" });
+
+  const updateClub = { ...clubs[clubIndex], ...req.body };
+  clubs[clubIndex] = updateClub;
+  res.json(updateClub);
+});
+
+app.delete("/clubs/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const clubIndex = clubs.findIndex((c) => c.id === id);
+
+  if (clubIndex === -1)
+    return res.status(404).json({ message: "Club Not Found" });
+
+  const deleteCLub = clubs.splice(clubIndex, 1);
+  res.json({ message: "Club deleted", data: delete [0] });
+});
+
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on 0.0.0.0:3000");
+});
